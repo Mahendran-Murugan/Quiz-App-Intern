67fc2cbf4db879d8f45f3338af7f743dfe98bd57
daniel <d.danielprince2003@gmail.com>
2024-06-25 21:30:11 +0530

    feat:Added update query feature to admin panel

--- Backend/Model/adminModel.js
+++ Backend/Model/adminModel.js
@@ -18,7 +18,6 @@ const createQuiz = (req, res, call) => {
           }
 
           body.questions.map((e) => {
-
             connection.query(
               `insert into question( quizid , question , ch1 , ch2 , ch3 , ch4 , answer , image , points) values (${result.insertId} , "${e.question}" ,"${e.ch1}", "${e.ch2}" ,"${e.ch3}" ,"${e.ch4}", "${e.answer}" ,"${e.image}" ,${e.points})`,
               (err, result, field) => {
@@ -48,21 +47,41 @@ const createQuiz = (req, res, call) => {
   }
 };
 
-
-const deleteQuiz = (req ,res)=>{
-    connection.query(`delete from quizz where id=${req.params.id}`, (err , result , field)=>{
-        if(err){
+const deleteQuiz = (req, res) => {
+  connection.query(
+    `delete from quizz where id=${req.params.id}`,
+    (err, result, field) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      connection.query(
+        `delete from question where quizid=${req.params.id}`,
+        (err1, result1, field1) => {
+          if (err1) {
             console.log(err);
             return;
+          }
+          res.json({ status: "good" });
         }
-        connection.query(`delete from question where quizid=${req.params.id}`, (err1 , result1 , field1)=>{
-            if(err1){
-                console.log(err);
-                return;
-            }
-            res.json({"status" : "good"})
-        })         
-    })
-}
+      );
+    }
+  );
+};
 
-module.exports = { createQuiz  , deleteQuiz };
+const updateQuestion = (req, res) => {
+  const e = req.body;
+  connection.query(
+    `update question set quizid = ${e.quizid} ,question= "${e.question}" ,ch1="${e.ch1}",ch2= "${e.ch2}" ,ch3="${e.ch3}" ,ch4="${e.ch4}", answer="${e.answer}" ,image="${e.image}" ,points=${e.points} where id = ${req.params.id}`,
+    (err, r, f) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.json({ status: "good" });
+      return;
+    }
+  );
+};
+
+module.exports = { createQuiz, deleteQuiz, updateQuestion };
--- Backend/Router/adminRouter.js
+++ Backend/Router/adminRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createQuiz, deleteQuiz } = require("../Model/adminModel");
+const { createQuiz, deleteQuiz, updateQuestion } = require("../Model/adminModel");
 const router = express.Router();
 const upload = require("../Model/imageStorage");
 router.post("/quiz/create", createQuiz);
 router.get("/quiz/:?" , )
 router.delete("/quiz/delete/:id" , deleteQuiz)
+router.put("/update/question/:id",updateQuestion)
 
 module.exports = { router };
