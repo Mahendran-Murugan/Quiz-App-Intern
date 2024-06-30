0d7f4959992e0a0636b9bd00d9b046af4223fc38
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-30 19:06:15 +0530

    feat: add switch tab prevention

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { store } from "../app/store";
 export const Quiz = () => {
+
+  let count = 0;
 
   const navigate = useNavigate();
 
@@ -11,11 +13,29 @@ export const Quiz = () => {
     if (document.visibilityState == 'hidden') navigate('/');
   }
 
+  const handleForbiddenKeys = (e) => {
+    if (e.keyCode === 9 || e.keyCode === 91 || e.keyCode === 18 || e.keyCode === 17 || e.keyCode === 27) navigate('/');
+    if (!document.fullscreenElement) navigate('/');
+  }
+
+  const fullscreenChanged = (e) => {
+    e.preventDefault();
+    count++;
+    console.log(count);
+    if (count > 1) navigate('/');
+  }
+
   useEffect(() => {
     document.addEventListener('visibilitychange', handleVisibilityChange);
 
+    document.addEventListener('keydown', handleForbiddenKeys);
+
+    document.addEventListener('fullscreenchange', fullscreenChanged);
+
     return () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange);
+      document.removeEventListener('keydown', handleForbiddenKeys);
+      document.removeEventListener('fullscreenchange', fullscreenChanged);
     }
   }, [])
 
