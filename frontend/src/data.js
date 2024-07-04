c3745751ae58bb62ad9415962d22782426d0b8ab
danielprinced <d.danielprince2003@gmail.com>
2024-07-04 11:00:03 +0530

    feat:Deploy Frontend

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,5 +1,5 @@
-const ADMIN_SERVER = "http://localhost:7000/api/admin";
-const USER_SERVER = "http://localhost:7000/api/user";
-const FILE_SERVER = "http://localhost:4000";
+const ADMIN_SERVER = "https://quiz-app-intern-1.onrender.com/api/admin";
+const USER_SERVER = "https://quiz-app-intern-1.onrender.com/api/user";
+const FILE_SERVER = "https://fileserver-jah8.onrender.com";
 const ADMIN_MAIL = "admin@gmail.com";
 module.exports = { ADMIN_SERVER, FILE_SERVER, USER_SERVER, ADMIN_MAIL };
