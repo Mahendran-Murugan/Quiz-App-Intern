4e40a34330a7d7e0d6b31c1056b8e24e9b694c91
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-07-05 10:21:24 +0530

    feat: modify auth for identify superuser

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOSTNAME ,
+  host: process.env.DATABASE_HOSTNAME,
   database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USERNAME ,
-  password: process.env.DATABASE_PASSWORD ,
-  port : 3306
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: 3306
 });
 
 module.exports = connection;
